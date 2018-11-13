b3f4a15a20f34bafbdd3b2391a216787bf466085
mackcellon <31675891+mackcellon@users.noreply.github.com>
2018-11-13 12:14:34 +0900

    Add files via upload

--- Renderer.cpp
+++ Renderer.cpp
@@ -39,47 +39,87 @@ void UpdateFrame(void)
 	// Draw
 	SetColor(255, 0, 0);
 
-	// Draw a filled circle with radius 100
+
+	//Startpoint
+	Vector3 StartPoint(0.0f, 0.0f);
+
+	//Circle Matrixes
+	Matrix3 CircleTrs;
+	Matrix3 CircleRot;
+
+	CircleTrs.SetTranslation(200.0f, 0.0f);
+
+	static float CircleDgree = 0;
+	CircleDgree += 1;
+	CircleDgree = fmodf(CircleDgree, 360.0f);
+	CircleRot.SetRotation(CircleDgree);
+
+	//CircleColor
+	int CircleColor = (int)CircleDgree/40 + 20;
+	SetColor(CircleColor * 34, CircleColor * 50, CircleColor * 1);
+
+	// CircleDraw
 	Vector3 center(0.0f, 0.0f);
-	float radius = 100.0f;
+	float radius = 30.0f;
 	int nradius = (int)radius;
 
-	static float degree = 0;
-	degree += 0.1f;
+	for (int i = -nradius; i <= nradius; i++)
+	{
+		for (int j = -nradius; j <= nradius; j++)
+		{
+			Vector3 vertex(i, j);
+			if (Vector3::Dist(center, vertex) <= radius)
+			{
+				PutPixel(Vector3(i, j)*CircleTrs*CircleRot);
+			}
+		}
+	}
+
+	//SquareMatix
+	Matrix3 SqureRot;
+	float Sradius = 50.0f;
+	int Snradius = (int)Sradius;
+	static float SquareDgr = 0;
+	SquareDgr += 0.5;
+	SquareDgr = fmodf(SquareDgr, 360.0f);
+	SqureRot.SetRotation(SquareDgr);
+
+	float maxScale = 1;
+	float scale = ((sinf(Deg2Rad(SquareDgr * 2)) + 1) * 0.5) * maxScale;
+	if (scale < 0.5f) scale = 0.5f;
+
+	Matrix3 SquareScl;
+	SquareScl.SetScale(scale, scale, scale);
+
+	int SqaureColor = (int) SquareDgr / 10 + 5;
+	SetColor(SqaureColor * 40, SqaureColor * 20, SqaureColor * 1);
+
+	//SquareDraw
+	for (int i = -Snradius; i <= Snradius; i++)
+	{
+		for (int j = -Snradius; j <= Snradius; j++)
+		{
+			PutPixel(Vector3((float)i, (float)j)*SqureRot*SquareScl);
+		}
+	}
+	/*static float degree = 0;
+	degree += 1;
 	degree = fmodf(degree, 360.0f);
 
 	Matrix3 rotMat;
 	rotMat.SetRotation(degree);
 	rotMat.Transpose();
 
-	static float maxscale = 3;
-	static float scale = ((sinf(Deg2Rad(degree * 2)) + 1) *0.5) * maxscale;
-	if (scale < 0.5)
-	{
-		scale = 0.5f;
-	}
+	
 
-	Matrix3 scaleMat;
-	scaleMat.SetScale(scale, scale, scale);
-
-	static float move = 1;
-	move += 1;
-	float theta = fmodf(move, 360);
-	float pos = sinf(Deg2Rad(theta))*150;
-	//move = fmodf(move, 150);
+	float maxPos = 150;
+	float pos = sinf(Deg2Rad(degree)) * maxPos;
 	Matrix3 translationMat;
 	translationMat.SetTranslation(pos, pos);
 
 	Matrix3 SR = scaleMat * rotMat;
-	Matrix3 TRS = translationMat * rotMat  *  scaleMat;
-
-	for (int i = -nradius; i <= nradius; i++)
-	{
-		for (int j = -nradius; j <= nradius; j++)
-		{
-			PutPixel(Vector3((float)i, (float)j) * TRS);
-		}
-	}
+	Matrix3 TRS = translationMat * rotMat * scaleMat;
+	*/
 
 	// Buffer Swap 
 	BufferSwap();
